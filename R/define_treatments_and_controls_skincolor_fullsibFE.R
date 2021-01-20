2cecb615c711d32d7edf7eecb96d0d8afe82e311
Wenjia Xu <xu@jccompute.uzh.ch>
2021-01-20 16:58:15 +0100

    fixed effect modelling treatment controls outcome

---
+++ R/define_treatments_and_controls_skincolor_fullsibFE.R
@@ -0,0 +1,99 @@
+define_treatments_and_controls_skincolor_fullsibFE = function(){ 
+  
+  ############################################################
+  # DEFINE "TREATMENTS", CONTROLS, OUTCOMES (INCLUDING TFBMs) 
+  ############################################################
+  
+  # TABLE 1
+  table1 =
+    c(
+      "CVD_mRNA",
+      "diabetes_mRNA",
+      "inflam1k_mRNA",
+      # "breast_cancer_mRNA",
+      # "Lupus_mRNA", "Colorectal_mRNA",
+      "Rheumatoid_Arthritis_mRNA", "Alzheimers_mRNA",
+      "Aortic_Aneurysm_mRNA", "COPD_mRNA",
+      "Asthma_mRNA","Hypertension_mRNA",
+      "Depression_mRNA",
+      "CKD_mRNA",
+      "aging_mRNA",
+      "aging_up_mRNA",
+      "aging_down_mRNA"
+    )
+  
+  treatment = c("color_byinterviewer_continuous",
+                "color_byinterviewer_binary")
+  
+  # check controls for each analysis!!!!!!!!!
+  basic = 
+    c(
+      "sex_interv", "famid_fullsib",
+      "Plate", "AvgCorrelogram100" ,"age_w5",
+      # over-representation 
+      "NK.cells.activated",
+      "T.cells.CD8",
+      # under-representation
+      "Macrophages.M0", 
+      "Macrophages.M2",
+      "B.cells.naive",
+      "T.cells.CD4.memory.resting"
+    )
+  race = c("raceethnicity_NonHblack",
+           "raceethnicity_Hispanic", basic)
+  
+  # ses = c("ses_sss_composite", basic)
+  # 
+  # ses_race = c("raceethnicity_NonHblack",
+  # "raceethnicity_Hispanic", "ses_sss_composite", basic)
+  controls = list(basic = basic, race = race
+                  # ses = ses,  ses_race = ses_race
+  )
+  
+  gene_set_name = signature_names %>% append("whole_genome_and_tfbm") 
+  
+  args = crossing(treatment, gene_set_name, controls)
+  
+  
+  for(i in 1:dim(args)[1]){
+    args$controls[i] = args$controls[i] %>% map(setdiff, args$treatment[i])
+  }
+  
+  
+  mediators = 
+    c(
+      "stress_perceived_lm",
+      "bills_binary",
+      "currentsmoke_binary",
+      "w5bmi_lm",
+      "insurance_lack_binary",
+      "lowbirthweight_binary",
+      "high_lowbirth_binary",
+      
+      "totdiscrim2_binary",  # binary
+      "discrim2_binary",   # binary
+      "totdiscrim1_category",   # categorical of 4
+      # special treatment in mediation
+      "totdiscrim1_gamma", #exponential fit glm for mediation
+      "totdiscrim2_gamma", #exponential fit glm for mediation
+      "countdiscrimwhy_gamma"#exponential fit glm for mediation
+      
+      # "totdiscrim1_pois", #poisson fit glm for mediation
+      # "totdiscrim2_pois", #poisson fit glm for mediation
+      # "countdiscrimwhy_pois"#poisson fit glm for mediation
+      
+      # # "totdiscrim1_category", #ordered logistic fit glm for mediation
+      # "totdiscrim2_category", #ordered logistic fit glm for mediation
+      # "countdiscrimwhy_category"#ordered logistic fit glm for mediation
+    )
+  
+  immune_tfbms = 
+    c(
+      "CEBPG_CREB3L1", "CREB3", "CREB3L1", "IRF2", "IRF3",
+      "IRF4", "IRF5", "IRF7", "IRF8", "IRF9", "JUN", "NFKB1", 
+      "NFKB2", "NR3C1"
+    )
+  
+  list2env(mget(ls()), .GlobalEnv)
+  
+}
