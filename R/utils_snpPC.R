718b305edd16b410dc6c8b417f8ac92883bbe059
Wenjia Xu <xu@jccompute.uzh.ch>
2021-02-16 14:19:34 +0100

    correct a bug where the obtained snp PCA dim is less than the default 20 enable to get ancestry PCs for the whole genome(treat the whole genome as a new signature) add two examples to show how to get PCs for future new signatures

--- R/utils_snpPC.R
+++ R/utils_snpPC.R
@@ -28,23 +28,30 @@ get_PC_dim_general <- function(gene_set_name) {
 # eqtl_pca is used to pre calculate all the pca as it takes at least 20 minitues for calculating one case and
 # it does not work within the pipeline, possibly because of the long waiting time
 eqtl_pca <- function(gene_set_name, p_eqtl) {
-  signatures <- readRDS("/home/share/preprocessed_two_batches/dt_batches1_2_steve_waves_17.11.2020_signature.rds")
-  gene_sets <- signatures$outcome_set %>% pluck(gene_set_name)
+  if (gene_set_name=="whole_genome"){
+    # for the whole genome
+    dat <- readRDS("/home/share/preprocessed_two_batches/dt_batches1_2_steve_waves_17.11.2020.rds")
+    gene_sets <- Biobase::featureNames(dat)
+  }else{
+    # for a predefined signature 
+    signatures <- readRDS("/home/share/preprocessed_two_batches/dt_batches1_2_steve_waves_17.11.2020_signature.rds")
+    gene_sets <- signatures$outcome_set %>% pluck(gene_set_name)
+  }
   G_list <- readRDS("/home/share/data_input/genename_15062020.rds")
   # specific gens of our interest
   gene <- G_list %>%
     filter(hgnc_symbol %in% gene_sets) %>%
     pull(ensembl_gene_id)
   # eqtl catelog
-  dat <- readRDS("/home/share/dna_ancestry/eqtl/whole.blood.rds")
+  catelog <- readRDS("/home/share/dna_ancestry/eqtl/whole.blood.rds")
   # filter Snps according to some p value threshold p_eqtl
-  dat <- dat[dat$pvalue < p_eqtl, ]
-  dat <- dat %>% filter(gene_id %in% gene)
+  catelog <- catelog[catelog$pvalue < p_eqtl, ]
+  catelog <- catelog %>% filter(gene_id %in% gene)
 
   # save Snps which are assoicated with specific genes passing p_eqtl
-  data.table::fwrite(unique(dat$rsid) %>% as.data.frame(),
-    file = str_c("/home/share/dna_ancestry/dna/keep", gene_set_name, "_", p_eqtl, ".txt"),
-    col.names = F
+  data.table::fwrite(unique(catelog$rsid) %>% as.data.frame(),
+                     file = str_c("/home/share/dna_ancestry/dna/keep", gene_set_name, "_", p_eqtl, ".txt"),
+                     col.names = F
   )
 
   # keep the snps from data set
@@ -55,10 +62,45 @@ eqtl_pca <- function(gene_set_name, p_eqtl) {
   system(str_c("plink --bfile /home/share/dna_ancestry/dna/kept", gene_set_name, "_", p_eqtl, ".omni --pca --out /home/share/dna_ancestry/dna/kept", gene_set_name, "_", p_eqtl, ".omni.pca"))
   # output will contain a kept......pca.eigenvec file with family ID, AID, then 20 PCs
 
+  eigenvalue <- data.table::fread(str_c("/home/share/dna_ancestry/dna/kept", gene_set_name, "_", p_eqtl, ".omni.pca.eigenval"))
+  ancestryPC_dim = dim(eigenvalue)[1]
+  ancestryPC_names <- str_c("AncestryPC", c(1:ancestryPC_dim))
+  
   custom_PCA = read.table(str_c("/home/share/dna_ancestry/dna/kept", gene_set_name, "_", p_eqtl,".omni.pca.eigenvec"),
-                          col.names = c("fid", "AID", "AncestryPC1", "AncestryPC2", "AncestryPC3", "AncestryPC4", "AncestryPC5",
-                                        "AncestryPC6", "AncestryPC7", "AncestryPC8", "AncestryPC9", "AncestryPC10", "AncestryPC11",
-                                        "AncestryPC12", "AncestryPC13", "AncestryPC14", "AncestryPC15", "AncestryPC16", "AncestryPC17",
-                                        "AncestryPC18", "AncestryPC19", "AncestryPC20"))
+                          col.names = c("fid", "AID", ancestryPC_names))
   custom_PCA %>% saveRDS(str_c("/home/share/dna_ancestry/dna/custom_PCA_", gene_set_name, "_", p_eqtl,".rds"))
-  }
+}
+
+
+# examples to get bespoke pca for new signatures and the whole genome:
+if(0){
+  gene_set_name = c(
+    "aging_down_cl1_mRNA",
+    "aging_down_cl1a_mRNA",
+    "aging_down_cl1b_mRNA",
+    "aging_down_cl1c_mRNA",
+    "aging_down_cl2_mRNA",
+    "aging_down_cl3_mRNA",
+    "aging_up_cl1_mRNA",
+    "aging_up_cl2_mRNA",
+    "aging_up_cl3_mRNA",
+    "aging_up_cl4_mRNA"
+    )
+
+  p_eqtl <- c(0.05, 1e-2, 1e-3, 1e-4, 1e-5, 1e-6, 1e-7, 1e-8, 1e-9, 1e-10)
+
+  args =
+    crossing(gene_set_name, p_eqtl)
+  args %>%
+    pmap(eqtl_pca)
+}
+
+if(0){
+  gene_set_name = "whole_genome"
+  p_eqtl <- c(0.05, 1e-2, 1e-3, 1e-4, 1e-5, 1e-6, 1e-7, 1e-8, 1e-9, 1e-10)
+  args =
+    crossing(gene_set_name, p_eqtl)
+  plan(multicore, workers = 10)
+  args %>%
+    furrr::future_pmap(eqtl_pca)
+}
